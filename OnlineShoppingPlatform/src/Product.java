8bbe55dabeb9ea25ff3b2c23413a05fd2d407bc8
Christopher Ryan banner <christopher.banner@snhu.edu>
2025-07-04 08:04:36 -0500

    new

---
+++ OnlineShoppingPlatform/src/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    String name;
+    double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public void displayInfo(){
+        System.out.println("Product " + name +  ", Price: $" + price);
+    }
+}
